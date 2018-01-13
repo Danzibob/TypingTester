4a5809edbf8373edb6fa933f0b6df15e221417b8
Danzibob <danzibob@gmail.com>
2018-01-13 15:05:38 +0000

    texts in seperate file, added keylogging

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-let text = "Test 123 test blah blah blah woo more text"
+let i = Math.floor(Math.random() * 2)
 let startTime = undefined
+let keys = ""
 const shiftThreshold = 12
-$(function(){
+$(window).on("load", function() {
+  setup(texts[i])
+})
+
+$(document).on('keydown', function(e) {
+  let key = event.keyCode || event.charCode
+  if (32 <= key && key < 90) {
+    keys += String.fromCharCode(key)
+    return
+  }
+  switch (key) {
+    case 8:
+      keys += "<"
+      break
+    case 192:
+      keys += "'"
+      break
+    case 13:
+    case 14:
+    case 15:
+    default:
+      break
+  }
+});
+
+function setup(text) {
+  //Reset things
+  $("#Legend").html("")
+  startTime = undefined
+  keys = ""
+
   // Set the content of the text to be typed
   let target = text
   $("#TypeThis").html(splitTextToSpans(target))
 
   let usedWidth = $("#TextEntry").width()
-  let docWidth = $( document ).width();
-  let scaleUp = (docWidth-20) / usedWidth
+  let docWidth = $(document).width()
+  let scaleUp = (docWidth - 20) / usedWidth
   let fontSize = $(".typing").first().css("font-size")
   console.log(fontSize)
-  $(".typing").each(function(){
+  $(".typing").each(function() {
     console.log($(this))
-    $(this).css("font-size", fontSize*scaleUp)
+    $(this).css("font-size", fontSize * scaleUp)
   })
   // When the user makes a change
-  $("#TextEntry").bind("input", function(){
+  $("#TextEntry").val("").bind("input", function() {
     let val = $("#TextEntry").val()
 
     // If timer isn't started, then start it
@@ -26,49 +57,64 @@ $(function(){
     }
 
     // If the user has finished, display their time
-    if (val == target){
-      let time = new Date().getTime() -startTime
-      alert("Your time: "+(time/1000)+"seconds")
+    if (val == target) {
+      let time = new Date().getTime() - startTime
+      finished(time)
     }
 
     // For each character...
     // (this is neccessary because of autocomplete)
-    for (let i=0; i<target.length; i++) {
+    for (let i = 0; i < target.length; i++) {
       // ...decide what color it should be,
       let color = "#FFFFFF"
-      if (val[i] == target[i]){
+      if (val[i] == target[i]) {
         color = "#4CAF50"
       } else if (val[i] !== undefined) {
         color = "#F44336"
       }
       // and assign that color
-      $("#char-"+i).css("background-color", color)
+      $("#char-" + i).css("background-color", color)
     }
 
     // Show and hide letters in order to shift along
-    for (let i=0; i<val.length-shiftThreshold; i++) {
-      $("#char-"+i).hide()
+    for (let i = 0; i < val.length - shiftThreshold; i++) {
+      $("#char-" + i).hide()
     }
-    for (let i=val.length-shiftThreshold; i<target.length; i++) {
-      $("#char-"+i).show()
+    for (let i = val.length - shiftThreshold; i < target.length; i++) {
+      $("#char-" + i).show()
     }
-  })
+  }).focus()
 
   // If the user tries to cheat
-  $("#TextEntry").bind("paste",function(){
+  $("#TextEntry").bind("paste", function() {
     alert("Oi. That's cheating.")
   })
-})
+}
 
-function splitTextToSpans(text){
+function splitTextToSpans(text) {
   let letters = text.split("")
   let newContents = ""
-  for (let i=0; i<letters.length; i++) {
-    newContents += "<span class='single-char' id='char-" + i + "'>"+letters[i]+"</span>"
+  for (let i = 0; i < letters.length; i++) {
+    newContents += "<span class='single-char' id='char-" + i + "'>" + letters[i] + "</span>"
   }
   return newContents
 }
 
-function checkInput(target, actual){
-  
+
+
+function finished(time) {
+  // Show the user a message with their time
+  console.log(keys)
+  $("#message").html("Completed!")
+  $("#sub-message").html("Your time: " + (time / 1000) + " seconds")
+  $("#TyperContainer").hide()
+  $("#message-box").show()
+  $("#nextText").on("click", function() {
+    // Switch the text and reset the page
+    i = (i + 1) % 2
+    setup(texts[i])
+    $("#TyperContainer").show()
+    $("#message-box").hide()
+    $("TextEntry").show().focus()
+  })
 }
---
+++ texts.js
@@ -0,0 +1,6 @@
+const texts = [
+  // "A passage is an excerpt from a piece of literature, or the entire piece. For example, in school you are sometimes asked to write an essay using a passage from a book. A passage might also be an article from a magazine or news story. A paragraph is a group of sentences joined to express an idea or similar ideas.",
+  // "There are two types of auctions: those in which the price starts out low and is gradually raised, and those in which the price starts out high and is gradually lowered. Both auction types have variants, such as a \"buyout option\" in which a buyer is guaranteed an object if they are willing to pay a high price."
+  "This is a test.",
+  "I'm another test."
+]
