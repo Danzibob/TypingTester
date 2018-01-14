d4df5fdbd0b85b2e1c3abf2b4817f63dc602fae6
root <root@vps503534.ovh.net>
2018-01-14 21:44:29 +0100

    Stuff.

--- public/texts.js
+++ public/texts.js
@@ -1,6 +1,8 @@
 const texts = [
   // "A passage is an excerpt from a piece of literature, or the entire piece. For example, in school you are sometimes asked to write an essay using a passage from a book. A passage might also be an article from a magazine or news story. A paragraph is a group of sentences joined to express an idea or similar ideas.",
   // "There are two types of auctions: those in which the price starts out low and is gradually raised, and those in which the price starts out high and is gradually lowered. Both auction types have variants, such as a \"buyout option\" in which a buyer is guaranteed an object if they are willing to pay a high price."
-  "This is a test.",
-  "I'm another test."
+  //  "This is a test.",
+  //  "I'm another test."
+  "We are having real difficulty trying to come up with a good text to use for our autocomplete experiment. We haven't even started on the second one yet!",
+  "So, here's the second text. You may have got it first because they're randomly ordered, but this one was written second. It's also slightly shorter"
 ]
