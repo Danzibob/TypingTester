891e33125ea28afc71f2fc60bffc47cc6120476d
Danzibob <danzibob@gmail.com>
2018-01-20 13:46:20 +0000

    Remove those goddamn apple apostrophes

--- public/texts.js
+++ public/texts.js
@@ -5,6 +5,6 @@ const texts = [
 	//  "I'm another test."
 	//"We are having real difficulty trying to come up with a good text to use for our autocomplete experiment. We haven't even started on the second one yet!",
 	//"So, here's the second text. You may have got it first because they're randomly ordered, but this one was written second. It's also slightly shorter"
-	"After that we decided to go to the cinema and watch a film, there wasn’t a lot to choose from and we ended up watching Paddington Bear. The film was surprisingly good and even made me cry a little. For dinner we went to Nando’s around 7 at night and I had plain chicken with chips. After that, we went back home to University, it was a very fun day.",
-	"Today I went out for the day in York with my friends. We got the bus early in the morning around 9 and decided to walk around the markets. Then we went to the Harry Potter shops had a look around, I decided not to buy anything but my friend decided she’d buy a gift for her friend. Next we went for lunch and I had a duck wrap which was very nice."
+	"After that we decided to go to the cinema and watch a film, there wasn't a lot to choose from and we ended up watching Paddington Bear. The film was surprisingly good and even made me cry a little. For dinner we went to Nando's around 7 at night and I had plain chicken with chips. After that, we went back home to University, it was a very fun day.",
+	"Today I went out for the day in York with my friends. We got the bus early in the morning around 9 and decided to walk around the markets. Then we went to the Harry Potter shops had a look around, I decided not to buy anything but my friend decided she'd buy a gift for her friend. Next we went for lunch and I had a duck wrap which was very nice."
 ]
